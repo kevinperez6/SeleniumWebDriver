89d81a96be44e82e5a9aa7897b9e36d138ee33ea
KevinPerez <73122734+kevinperez6@users.noreply.github.com>
2023-03-25 15:24:44 -0600

    Entorno de desarrollo, WebDriver

---
+++ src/main/java/Test/Test.java
@@ -0,0 +1,26 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Test {
+    public static void main(String[] args) {
+        /*_____________________________________________*/
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        /*_____________________________________________*/
+
+        //Inicialize browser
+        System.setProperty("webdriver.chrome.driver","E:\\Programas\\ChromeDriver\\chromedriver.exe");
+
+        //OPEN PAGE
+        driver.get("http://www.facebook.com");
+
+        //MAXIMIZE BROWSER
+        driver.manage().window().maximize();
+        driver.close();
+
+    }
+}
