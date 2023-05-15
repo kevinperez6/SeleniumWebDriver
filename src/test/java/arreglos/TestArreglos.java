9b5d0edf00c1d2934fd5482aba77b7003bbb6250
KevinPerez <73122734+kevinperez6@users.noreply.github.com>
2023-05-14 20:54:26 -0500

    arreglos

--- src/test/java/arreglos/TestArreglos.java
+++ src/test/java/arreglos/TestArreglos.java
@@ -1,14 +1,35 @@
 package arreglos;
 
+import personaArreglos.Persona;
+
 public class TestArreglos {
     public static void main(String[] args) {
-        int edades[] = new int[3];//Un arreglo es un objeto
+        /*int edades[] = new int[3];//Un arreglo es un objeto
         System.out.println("Edades: " + edades);
 
         edades[0] = 10;
         edades[1] = 20;
         edades[2] = 30;
 
-        System.out.println("Edades posición 0 = " + edades[0]);
+        for(int i = 0; i < edades.length; i++){
+            System.out.println("Edades contenidas: " + edades[i]);
+        }*/
+
+        Persona personas[] = new Persona[2];
+        personas[0] = new Persona("Juan");
+        personas[1] = new Persona("Carlos");
+
+
+        System.out.println("Persona 0 = " + personas[0]);
+        System.out.println("Persona 0 = " + personas[1]);
+
+        for(int i = 0; i <personas.length; i++){
+            System.out.println("Personas =" + personas[i]);
+        }
+        String frutas[] = {"Naranja","Pera","Uva"};
+        System.out.println("frutas[0] = " + frutas[0]);
+        for (int i = 0; i < frutas.length; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
